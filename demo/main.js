834d679d20b6b41bd1635ef282f4849688e3e0ff
Angelina Fabbro <cyborg@digitalocean.com>
2015-05-23 18:39:05 +1000

    mash button, get sound

--- demo/main.js
+++ demo/main.js
@@ -23,3 +23,26 @@ node.ready().then(function(){
 	node.playSample();
 });
 
+var gamepad;
+var buttonStates;
+
+window.addEventListener("gamepadconnected", function(e){
+	gamepad = e.gamepad;
+	buttonStates = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  console.log(e.gamepad);
+  setInterval(checkButtons, 100);
+});
+
+function checkButtons() {
+	for (var i = gamepad.buttons.length - 1; i >= 0; i--) {
+		if (buttonStates[i] != gamepad.buttons[i].pressed) {
+			buttonStates[i] = gamepad.buttons[i].pressed;
+			if (gamepad.buttons[i].pressed) {
+				node.playSample();
+			};
+		};
+	};
+};
+
+
+
